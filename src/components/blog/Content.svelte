b495dc3ea928e24bc4096c8986f8a276e6a3a429
Agent-Kwabbel <FabeStuffken@proton.me>
2024-06-26 21:35:13 +0200

    Moved blog content to Content.svelte

---
+++ src/components/blog/Content.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    export let content: String;
+</script>
+
+<div class="blog_content">
+    {@html content}
+</div>
+
+<style>
+    .blog_content {
+        margin: 1rem 0 6rem 0;
+        font-size: 1rem;
+
+        & h4 {
+            font-size: 1.5rem;
+            margin: .5rem 0;
+        }
+
+        & h5 {
+            font-size: 1.25rem;
+            margin: .5rem 0;
+        }
+
+        & h6 {
+            font-size: 1rem;
+            margin: .5rem 0;
+        }
+
+        & p {
+            margin: 0;
+            line-height: 1.25rem;
+        }
+
+        & blockquote {
+            margin: 1rem 0;
+            padding: 1rem;
+            border-left: 5px solid white;
+            font-style: italic;
+        }
+
+        /*Now place the image horizontaly in the middle of the content*/
+
+        & figure {
+            flex-direction: column;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            margin: 1rem 0;
+        }
+
+        & img {
+            width: 75%;
+            height: auto;
+        }
+
+        & video {
+            width: 75%;
+            height: auto;
+        }
+
+        & figcaption {
+            font-size: .75rem;
+            font-weight: 300;
+            margin-top: 0.5rem;
+            width: 75%;
+        }
+
+        & mark {
+            background-color: #fde112;
+            color: black;
+            padding: 0.05rem;
+        }
+
+        & a {
+            color: white;
+            text-decoration: underline;
+        }
+    }
+    </style>
